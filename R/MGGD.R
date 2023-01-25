b7e3db7abde518e76af13c96bef5ab4519bdbf78
wikanda phaphan <maji.hunter@gmail.com>
2023-01-25 16:23:08 +0700

    bug fix

--- R/MGGD.R
+++ R/MGGD.R
@@ -70,13 +70,13 @@ rMGG=function(n,lambda,beta,alpha,p){
 #rMGG(n,lambda,beta,alpha,p)
 #X<-rMGG(n,lambda,beta,alpha,p)
 
-EM=function(X,n,lambda,beta,alpha,p){
+EM=function(X,n){
   flcomplete1<-function(beta)
   {
-    
-    (n*(ak-1))*log(gamma(beta[3]+(1/beta[2])))+(n*((beta[3]*beta[2])-1-ak)*log(beta[1]))+(n*log(beta[2]))+sum(n*((beta[3]*beta[2])-1)*log(X))-sum((beta[1]*X)^beta[2])-(n*ak*log(gamma(beta[3]))) 
-    
-  } 
+
+    (n*(ak-1))*log(gamma(beta[3]+(1/beta[2])))+(n*((beta[3]*beta[2])-1-ak)*log(beta[1]))+(n*log(beta[2]))+sum(n*((beta[3]*beta[2])-1)*log(X))-sum((beta[1]*X)^beta[2])-(n*ak*log(gamma(beta[3])))
+
+  }
   ####################### find initial value#################################
   Xbar <-apply(X,2,mean)
   alpha.old <- 0.5/(log(Xbar)-mean(log(X)))
@@ -84,7 +84,7 @@ EM=function(X,n,lambda,beta,alpha,p){
   p.old <- 0.5
   #lambda.old <- 0.5
   lambda.old <- Xbar/(apply(X,2,var))
-  
+
   cond=1
   cond2=1
   cond3=1
@@ -100,13 +100,13 @@ EM=function(X,n,lambda,beta,alpha,p){
     beta.new<-jib$par[2]
     alpha.new<-jib$par[3]
     #p.new <-jib$par[4]
-    
+
     #cond=abs(p.new-p.old)+abs(lambda.new-lambda.old)+abs(beta.new-beta.old)+abs(alpha.new-alpha.old)
     cond=abs(p.new-p.old)
     cond2=abs(lambda.new-lambda.old)
     cond3=abs(beta.new-beta.old)
     cond4=abs(alpha.new-alpha.old)
-    
+
     p.old=p.new
     lambda.old=lambda.new
     beta.old=beta.new
@@ -114,5 +114,5 @@ EM=function(X,n,lambda,beta,alpha,p){
   }
   return(list(p.hat=p.new,lambda.hat=lambda.new,beta.hat=beta.new,alpha.hat=alpha.new))
 }
-#EM(X,n,lambda,beta,alpha,p)
+#EM(X,n)
 
